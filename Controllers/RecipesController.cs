9e48ef8a20dd80c6d448d7f7363c89b82a7175c8
Frandy Sena <frandysena2@gmail.com>
2026-07-28 20:22:34 -0400

    fix: agregados cambios pendientes en endpoints GET

--- Controllers/RecipesController.cs
+++ Controllers/RecipesController.cs
@@ -1,6 +1,27 @@
-﻿namespace RecetasApp.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using RecetasApp.Models;
+using RecetasApp.Services;
+
+namespace RecetasApp.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RecipesController : ControllerBase
 {
-    public class RecipesController
+    private readonly RecipeService _service;
+
+    public RecipesController(RecipeService service)
     {
+        _service = service;
+    }
+
+    [HttpPost]
+    public IActionResult Create([FromBody] Recipe recipe)
+    {
+        if (string.IsNullOrWhiteSpace(recipe.Title))
+            return BadRequest(new { message = "El título es obligatorio" });
+
+        _service.Add(recipe);
+        return Ok(recipe);
     }
 }
