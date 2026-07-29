780b0ccd3dd899b643c4f643069f1d7e101b3d5a
Frandy Sena <frandysena2@gmail.com>
2026-07-28 20:16:14 -0400

    feat: servicio InMemory y endpoint para crear recetas

---
+++ Services/RecipeService.cs
@@ -0,0 +1,27 @@
+﻿using RecetasApp.Models;
+
+namespace RecetasApp.Services;
+
+public class RecipeService
+{
+    private readonly List<Recipe> _recipes = new();
+
+    public IEnumerable<Recipe> GetAll() => _recipes;
+    public Recipe? GetById(string id) => _recipes.FirstOrDefault(r => r.Id == id);
+    public void Add(Recipe recipe) => _recipes.Add(recipe);
+
+    public bool Update(Recipe recipe)
+    {
+        var index = _recipes.FindIndex(r => r.Id == recipe.Id);
+        if (index == -1) return false;
+        _recipes[index] = recipe;
+        return true;
+    }
+
+    public bool Delete(string id)
+    {
+        var recipe = GetById(id);
+        if (recipe == null) return false;
+        return _recipes.Remove(recipe);
+    }
+}
